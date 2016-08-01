f6ec4611d0913f2c857dc19c05562a4ae62b428c
wangyh <wanianma@foxmail.com>
2016-08-01 11:19:14 +0800

    修改phantomjs处理文件

--- phantomjs/pro_youku.js
+++ phantomjs/pro_youku.js
@@ -1,1 +1,37 @@
-<?`:
+// Read the Phantom webpage '#intro' element text using jQuery and "includeJs"
+
+"use strict";
+var page = require('webpage').create(),
+    system = require('system'),
+    url;
+
+//page.viewportSize = {width: 4800,height: 8000};
+page.settings.loadImages = false;
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+
+if (system.args.length === 1) {
+    phantom.exit(1);
+} else {
+    url = system.args[1];
+    page.open(url, function(status) {
+    // Checks for bottom div and scrolls down from time to time
+    var count = 0;
+    window.setInterval(function() {
+      if(count != page.content.match(/class="yk-col4"/g).length) { // Didn't find
+        count = page.content.match(/class="yk-col4"/g).length;
+        page.evaluate(function() {
+          // Scrolls to the bottom of page
+          window.document.body.scrollTop = document.body.scrollHeight;
+        });
+      }
+      else { 
+        //page.render('youku.png');
+        console.log(page.content);
+        phantom.exit();
+      }
+    }, 1000);
+});
+}
+
